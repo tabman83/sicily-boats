05403cff1b4777d026812fe0f2b257f061ae3e55
Antonino Parisi <tabman83@gmail.com>
2018-07-11 10:30:57 +0200

    Read server port from environment.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 const app = require('./server/app');  
-const port = 5050;
+const port = process.env.PORT || 5050;
 
 app.listen(port, function() {  
     console.log('Express server listening on port ' + port);
